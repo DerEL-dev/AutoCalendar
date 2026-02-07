3493ce26445aaeec01196944c39df73f8e0a513e
Michael <michaelhaas584@gmail.com>
2026-02-07 13:52:30 +0100

    starting from scratch

--- scripts/popup.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("openPage").addEventListener("click", () => {
-    const url = chrome.runtime.getURL("calendar.html");
-    chrome.tabs.create({ url });
-  });
-});
